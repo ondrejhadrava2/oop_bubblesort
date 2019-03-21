7d8d70b63c02610ef03df97e9a4ad53ef382ee40
ondrejhadrava2 <48293941+ondrejhadrava2@users.noreply.github.com>
2019-03-21 12:01:22 +0100

    OOP Bubblesort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] inputField = new int[10];
+            for (int i = 0; i < inputField.Length; i++)
+            {
+                inputField[i] = random.Next(1, 100);
+            }
+            Sort a = new Sort(inputField);
+            a.Initialize();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BubbleSort/BubbleSort/Sort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Sort
+    {
+        public int[] input;
+
+        public Sort(int [] input)
+        {
+            this.input = input;
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Array before sorting: {0}", Visualize() + "\n");
+            Sorting();
+            Console.WriteLine("\nSorted array: {0}", Visualize());
+        }
+
+        private void Sorting()
+        {
+            bool unsorted = true;
+            while (unsorted)
+            {
+                unsorted = false;
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    if (input[i] > input[i + 1])
+                    {
+                        Switch(i);
+                        Console.WriteLine(Visualize());
+                        unsorted = true;
+                    }
+                }
+            }
+        }
+
+        private void Switch(int i)
+        {
+            int temp;
+            temp = input[i];
+            input[i] = input[i + 1];
+            input[i + 1] = temp;
+        }
+
+        private string Visualize()
+        {
+            string s = "";
+            foreach (int i in input)
+                s += String.Format("{0} ", i);
+            return s;
+        }
+
+
+    }
+}
